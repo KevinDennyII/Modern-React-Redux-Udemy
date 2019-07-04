e7b473469d87acb89e87b5178b3b13ed3f4d2a1a
Kevin Denny II <kevindennyii@iheartmedia.com>
2019-07-03 23:38:18 -0500

    adding the use of State...

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) =>  console.log(position),
-        (err) => console.log(err)
-    );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return <div>Mighty Mouse To Save The Day!</div>;
-};
+        // THIS IS THE ONLY TIME we do direct assignment
+        // to this.state
+        this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position =>  {
+                // we called setState to update our state project!!!!
+                this.setState({ lat: position.coords.latitude });
+                // WE DID NOT!!
+                // this.state.late = position.coords.latitude
+            },
+            err => console.log(err)
+        );
+    }
+
+    // React says we have to define render()
+    render() {
+        return <div>Latitude: {this.state.lat} </div>;
+    }
+}
 
 ReactDOM.render(
     <App />,
