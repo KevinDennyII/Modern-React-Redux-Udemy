014a482690dd09ee900fd0d8bdccfdee4a10548a
kevindennyii <kevin.dennyii@gmail.com>
2019-07-18 21:57:12 -0500

    more practice..

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,43 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from  './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    state = { lat: null, errorMessage:''};
 
-        // THIS IS THE ONLY TIME we do direct assignment
-        // to this.state
-        this.state = { lat: null, errorMessage: '' };
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position =>  {
-                // we called setState to update our state object!!!!
-                this.setState({ lat: position.coords.latitude });
-                // WE DID NOT!!
-                // this.state.late = position.coords.latitude
-            },
-            err => {
-                this.setState({
-                    errorMessage: err.message
-                })
-            }
+            // we called setState to update our state object!!!!
+            position => this.setState({ lat: position.coords.latitude }),
+            // WE DID NOT!!
+            // this.state.late = position.coords.latitude
+            err => this.setState({ errorMessage: err.message})
         );
     }
 
     // React says we have to define render()
     // render is called twice: once while the browser is loaded then the
-    // second time once the state object is updated
+    // second time once the state object is updated...CONDITIONAL RENDERING IS BEING SHOWN BELOW
     render() {
             // if we have an error
-            if (this.state.errorMessage && ! this.state.lat){
-                return <div>Error: {this.state.errorMessage} </div>
-            }
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage} </div>
+        }
             // no error
-            if (!this.state.errorMessage && this.state.lat){
-                return <div>Latitude: {this.state.lat} </div>
-            }
-            //
-            return
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}/>
+
+        }
+        //
+            return <div>Loading!...</div>;
     }
 }
 
