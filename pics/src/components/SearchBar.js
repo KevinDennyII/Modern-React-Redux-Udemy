aa49bd78e9861e0f8bf7bc8608bb86afcdf287f5
kevindennyii <kevin.dennyii@gmail.com>
2019-07-24 23:42:46 -0500

    finished with dicussing the THIS issues...

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+    state = { term: ''};
 
+    onFormSubmit = (event) => {
+        event.preventDefault(); // keep the form from trying to submit automatically
+
+        // this will not work, it thinks this is resolved to 'undefined'
+        //  console.log(this.state.term);
+    };
+
+    // <form onSubmit={(event) => this.onFormSubmit())} className="ui form">
+    // alternative way to solve the 'this' issue
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label htmlFor="">Image Search</label>
-                        <input type="text" onChange={this.onInputChange}/>
+                        <input type="text"
+                               value={this.state.term}
+                               onChange={e => this.setState({ term: e.target.value })}
+                        />
                     </div>
                 </form>
             </div>
