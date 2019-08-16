4383a0a7281a36075b68d06fb51be74ecc5b1cff
Kevin Denny II <kevindennyii@iheartmedia.com>
2019-08-16 11:11:11 -0500

    completed video search application with components

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('Mighty Mouse Cartoon')
+    }
+
     // asynchronous api request being made here...
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -15,7 +19,11 @@ class App extends React.Component {
             }
         });
 
-        this.setState({ videos: response.data.items })
+        //updating application...
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
 
     onVideoSelect = (video) => {
@@ -26,8 +34,19 @@ class App extends React.Component {
         return  (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList
+                                onVideoSelect={this.onVideoSelect}
+                                videos={this.state.videos}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
