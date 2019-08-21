ac557fc93a750b9fb9d6e659a60570f44480606c
Kevin Denny II <kevindennyii@iheartmedia.com>
2019-08-21 15:29:12 -0500

    adding more about the PostList.js

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -3,15 +3,33 @@ import { connect } from 'react-redux';
 import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
-
     componentDidMount() {
         this.props.fetchPosts();
     }
 
-    render() {
-        return <div>Post List</div>;
+    renderList(){
+        return this.props.posts.map(post => {
+            return (
+                <div className="item" key={post}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        });
     }
+
+    render() {
+        return <div className="ui relaxed divided list">{this.renderList()}</div>;
+    }
+}
+
+const mapStateToProps = state => {
+    return { posts: state.posts };
 };
 
-
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
