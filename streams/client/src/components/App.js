fd022b8578542be4f05bbd4201b3ab2c288fae2d
Kevin Denny II <kevindennyii@iheartmedia.com>
2019-09-13 14:03:48 -0500

    completed implementation of all streams

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamDelete from "./streams/StreamDelete";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
@@ -10,16 +10,20 @@ import history from '../history';
 
 const App = () => {
   // WE DO NOT WANT TO USE ANCHOR TAGS HERE!!! <A>
+
+  // using Switch so that ReactDOM only shows the appropriate Route it needs to show and not other routes/components
   return (
       <div className="ui container">
         <Router history={history}>
           <div>
             <Header />
-            <Route path="/" exact component={StreamList} />
-            <Route path="/streams/new" exact component={StreamCreate} />
-            <Route path="/streams/edit/:id" exact component={StreamEdit} />
-            <Route path="/streams/delete/:id" exact component={StreamDelete} />
-            <Route path="/streams/show" exact component={StreamShow} />
+            <Switch>
+                <Route path="/" exact component={StreamList} />
+                <Route path="/streams/new" exact component={StreamCreate} />
+                <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                <Route path="/streams/:id" exact component={StreamShow} />
+            </Switch>
           </div>
         </Router>
       </div>
